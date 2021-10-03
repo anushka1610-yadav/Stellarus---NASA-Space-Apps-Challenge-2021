bf65ae490299bb5ba7ebc400ac3fa3eb3cf02a5f
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2021-10-03 18:29:48 +0530

    Added the project.

---
+++ Stellarus/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+
+    bool isProvoked = false;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        } 
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else
+        {
+            AttackTarget();
+        }
+    }
+
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        print("Hi");
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange );
+    }
+
+}
