8907f44a2811798721f361c765fe748ebd4014eb
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2021-10-03 22:14:55 +0530

    Added Complete project.

---
+++ NASA/Assets/SceneManagement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.L))
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+}
